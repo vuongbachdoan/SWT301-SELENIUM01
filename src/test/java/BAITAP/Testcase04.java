7ff096bfc349b4a7f631f65ece51bf3782ec83e4
vuongbachdoan <vuongbachdoan@gmail.com>
2023-10-31 09:29:18 +0700

    feat: add tc05

--- src/test/java/BAITAP/Testcase04.java
+++ src/test/java/BAITAP/Testcase04.java
@@ -20,47 +20,95 @@ public class Testcase04 {
         // Step 1: Go to http://live.techpanda.org/
         driver.get("http://live.techpanda.org/");
 
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         // Step 2: Click on -> MOBILE -> menu
         WebElement mobileMenu = driver.findElement(By.linkText("MOBILE"));
         mobileMenu.click();
 
-        // Step 3: In mobile products list, click on -> Add To Compare -> for 2 mobiles (Sony Xperia & Iphone)
-        WebElement addToCompareSony = driver.findElement(By.xpath("//a[contains(text(),'Sony Xperia')]/following-sibling::div/button[contains(@title, 'Add to Compare')]"));
-        addToCompareSony.click();
-        
-        WebElement addToCompareIphone = driver.findElement(By.xpath("//a[contains(text(),'Iphone')]/following-sibling::div/button[contains(@title, 'Add to Compare')]"));
-        addToCompareIphone.click();
-
-        // Step 4: Click on -> COMPARE -> button. A popup window opens
-        WebElement compareButton = driver.findElement(By.cssSelector("button[title='Compare']"));
-        compareButton.click();
-        
-        // Switch to new window opened
-        for(String winHandle : driver.getWindowHandles()){
-            driver.switchTo().window(winHandle);
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        // Step 5: Verify the pop-up window and check that the products are reflected in it
-        WebElement heading = driver.findElement(By.cssSelector(".page-title title"));
-        
-        if (heading.getText().equals("COMPARE PRODUCTS")) {
+        // Step 3: In mobile products list, click on -> Add To Compare -> for 2 mobiles
+        // (Sony Xperia & Iphone)
+        WebElement addToCompareSony = driver.findElement(By
+                .xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div[1]/div[3]/ul/li[1]/div/div[3]/ul/li[2]/a"));
+        addToCompareSony.click();
+
+        WebElement addToCompareIphone = driver.findElement(By
+                .xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div[1]/div[3]/ul/li[2]/div/div[3]/ul/li[2]/a"));
+        addToCompareIphone.click();
+
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Step 4: Click on -> COMPARE -> button. A popup window opens
+        WebElement compareButton = driver
+                .findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[3]/div[1]/div[2]/div/button"));
+        compareButton.click();
+
+        // Store the ID of the original window
+        String originalWindow = driver.getWindowHandle();
+
+        // Switch to new window
+        for (String winHandle : driver.getWindowHandles()) {
+            if (!originalWindow.contentEquals(winHandle)) {
+                driver.switchTo().window(winHandle);
+                break;
+            }
+        }
+
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Step 5: Verify the pop-up window and check that the products are reflected in
+        // it
+        WebElement heading = driver.findElement(By.xpath("//*[@id=\"top\"]/body/div/div[1]/h1"));
+
+        if (heading.getText().equals("Compare Products")) {
             System.out.println("Heading is correct!");
         } else {
             System.out.println("Heading is incorrect!");
         }
-        
-        WebElement sonyProduct = driver.findElement(By.xpath("//h2/a[contains(text(),'Sony Xperia')]"));
-        WebElement iphoneProduct = driver.findElement(By.xpath("//h2/a[contains(text(),'Iphone')]"));
-        
+
+        WebElement sonyProduct = driver
+                .findElement(By.xpath("//*[@id=\"product_comparison\"]/tbody[1]/tr[1]/td[1]/h2/a"));
+        WebElement iphoneProduct = driver
+                .findElement(By.xpath("//*[@id=\"product_comparison\"]/tbody[1]/tr[1]/td[2]/h2/a"));
+
         if (sonyProduct.isDisplayed() && iphoneProduct.isDisplayed()) {
             System.out.println("Both products are reflected in the popup!");
         } else {
             System.out.println("One or both of the products are not reflected in the popup!");
         }
 
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         // Capture screenshot at the end
-        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
         try {
             FileUtils.copyFile(scrFile, new File("src/main/resources/screenshots/screenshot_tc04.png"));
             System.out.println("Screenshot captured!");
@@ -69,14 +117,13 @@ public class Testcase04 {
             System.out.println("Failed to capture screenshot!");
         }
 
-        // Close the Popup Windows
+        // Close the new window or tab
         driver.close();
 
-        // Switch back to original browser (first window)
-        String originalWindow = driver.getWindowHandle();
+        // Switch back to the original window
         driver.switchTo().window(originalWindow);
 
-        // Close the browser
+        // Close the original window
         driver.quit();
     }
 }
--- src/test/java/BAITAP/Testcase05.java
+++ src/test/java/BAITAP/Testcase05.java
@@ -20,73 +20,141 @@ public class Testcase05 {
         // Step 1: Go to http://live.techpanda.org/
         driver.get("http://live.techpanda.org/");
 
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         // Step 2: Click on my account link
         WebElement myAccountLink = driver.findElement(By.linkText("MY ACCOUNT"));
         myAccountLink.click();
 
-        // Step 3: Click Create an Account link and fill New User information excluding the registered Email ID.
-        WebElement createAccountLink = driver.findElement(By.linkText("CREATE AN ACCOUNT"));
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Step 3: Click Create an Account link and fill New User information excluding
+        // the registered Email ID.
+        WebElement createAccountLink = driver.findElement(By.xpath("//*[@id=\"login-form\"]/div/div[1]/div[2]/a"));
         createAccountLink.click();
-        
+
         // Fill New User information
-        LoginForm loginForm = new LoginForm(driver, "Test", "User", "idbciqabfci@gmail.com", "password123", "password123");
+        LoginForm loginForm = new LoginForm(driver);
+        loginForm.fillForm("Test", "Mid","User", "idbbvjciqabfci@gmail.com", "password123", "password123");
+
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         // Step 4: Click Register
-        WebElement registerButton = driver.findElement(By.cssSelector("button[title='Register']"));
+        WebElement registerButton = driver.findElement(By.xpath("//*[@id=\"form-validate\"]/div[2]/button"));
         registerButton.click();
 
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         // Step 5: Verify Registration is done. Expected account registration done.
-        WebElement successMessage = driver.findElement(By.cssSelector(".success-msg"));
-        
-        if (successMessage.getText().contains("Thank you for registering")) {
+        WebElement successMessage = driver
+                .findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div/div/ul/li/ul/li/span"));
+
+        if (successMessage.getText().contains("Thank you for registering with Main Website Store.")) {
             System.out.println("Registration is done!");
         } else {
             System.out.println("Registration failed!");
+        }
+
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
         // Step 6: Go to TV menu
         WebElement tvMenu = driver.findElement(By.linkText("TV"));
         tvMenu.click();
 
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         // Step 7: Add product in your wish list - use product - LG LCD
-        WebElement addToWishlistLG = driver.findElement(By.xpath("//a[contains(text(),'LG LCD')]/following-sibling::div/button[contains(@title, 'Add to Wishlist')]"));
+        WebElement addToWishlistLG = driver.findElement(By
+                .xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div[2]/div[1]/div[2]/ul/li[1]/div/div[3]/ul/li[1]/a"));
         addToWishlistLG.click();
 
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         // Step 8: Click SHARE WISHLIST
-        WebElement shareWishlistButton = driver.findElement(By.cssSelector("button[title='Share Wishlist']"));
+        WebElement shareWishlistButton = driver
+                .findElement(By.xpath("//*[@id=\"wishlist-view-form\"]/div/div/button[1]"));
         shareWishlistButton.click();
 
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         // Step 9: In next page enter Email and a message and click SHARE WISHLIST
-        WebElement emailInput = driver.findElement(By.id("email_address"));
+        WebElement emailInput = driver.findElement(By.xpath("//*[@id=\"email_address\"]"));
         emailInput.sendKeys("friend@example.com");
-        
-        WebElement messageInput = driver.findElement(By.id("message"));
+
+        WebElement messageInput = driver.findElement(By.xpath("//*[@id=\"message\"]"));
         messageInput.sendKeys("Check out my wishlist!");
-        
-        WebElement shareWishlistButton2 = driver.findElement(By.cssSelector("button[title='Share Wishlist']"));
+
+        WebElement shareWishlistButton2 = driver.findElement(By.xpath("//*[@id=\"form-validate\"]/div[2]/button"));
         shareWishlistButton2.click();
 
-         // Step 10: Check wishlist is shared. Expected wishlist shared successfully.
-         WebElement successMessage2 = driver.findElement(By.cssSelector(".success-msg"));
-        
-         if (successMessage2.getText().contains("Your Wishlist has been shared")) {
-             System.out.println("Wishlist is shared!");
-         } else {
-             System.out.println("Failed to share wishlist!");
-         }
+        // Add sleep
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-         // Capture screenshot at the end
-         File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        
-         try {
-             FileUtils.copyFile(scrFile, new File("src/main/resources/screenshots/screenshot_tc05.png"));
-             System.out.println("Screenshot captured!");
-         } catch (IOException e) {
-             e.printStackTrace();
-             System.out.println("Failed to capture screenshot!");
-         }
+        // Step 10: Check wishlist is shared. Expected wishlist shared successfully.
+        WebElement successMessage2 = driver.findElement(By.cssSelector(".success-msg"));
 
-         // Close the browser
-         driver.quit();
+        if (successMessage2.getText().contains("Your Wishlist has been shared.")) {
+            System.out.println("Wishlist is shared!");
+        } else {
+            System.out.println("Failed to share wishlist!");
+        }
+
+        // Capture screenshot at the end
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(scrFile, new File("src/main/resources/screenshots/screenshot_tc05.png"));
+            System.out.println("Screenshot captured!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed to capture screenshot!");
+        }
+
+        // Close the browser
+        driver.quit();
     }
 }
