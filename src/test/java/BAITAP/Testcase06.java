51f8e40b5c7c8d1f59b50d9308f8c1b97085b6f3
vuongbachdoan <vuongbachdoan@gmail.com>
2023-11-01 18:08:54 +0700

    feat: refactor code:

--- src/test/java/BAITAP/Testcase06.java
+++ src/test/java/BAITAP/Testcase06.java
@@ -3,17 +3,12 @@ package BAITAP;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.concurrent.TimeoutException;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -24,7 +19,28 @@ import POM.ShippingForm;
 import driver.driverFactory;
 
 public class Testcase06 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        By myAccountLink = By.linkText("MY ACCOUNT");
+        By loginBtn = By.cssSelector("#send2");
+        By myWishlistLink = By.linkText("MY WISHLIST");
+        By addToCartBtn = By.cssSelector("tr > td.wishlist-cell4.customer-wishlist-item-cart > div > button");
+        By estimateBtn = By.cssSelector("#shipping-zip-form > div > button");
+        By shippingCost = By.cssSelector("#co-shipping-method-form > dl > dd > ul > li > label > span");
+        By shippingCostSelector = By.cssSelector("#co-shipping-method-form > dl > dd > ul > li > label");
+        By shippingCostAddToTotal = By.cssSelector("#co-shipping-method-form > div > button");
+        By priceNet = By.cssSelector("#shopping-cart-totals-table > tbody > tr:nth-child(1) > td:nth-child(2) > span");
+        By priceTotal = By.cssSelector("#shopping-cart-totals-table > tfoot > tr > td:nth-child(2) > strong > span");
+        By checkoutProcessBtn = By.cssSelector(
+                "body > div > div > div.main-container.col1-layout > div > div > div > div.cart-totals-wrapper > div > ul > li.method-checkout-cart-methods-onepage-bottom > button");
+        By billingContinueBtn = By.cssSelector("#billing-buttons-container > button");
+        By shippingContinueBtn = By.cssSelector("#shipping-buttons-container > button");
+        By shippingMethodBtn = By.cssSelector("#shipping-method-buttons-container button");
+        By methodCheckoutMoney = By.cssSelector("#dt_method_checkmo > label");
+        By methodCheckoutMoneyContinue = By.cssSelector("#payment-buttons-container > button");
+        By placeOrderBtn = By.cssSelector("#review-buttons-container > button");
+        By orderNumber = By
+                .cssSelector("body > div > div > div.main-container.col1-layout > div > div > p:nth-child(3)");
+
         WebDriver driver = driverFactory.getChromeDriver();
         // Step 1: Go to http://live.techpanda.org/
         driver.get("http://live.techpanda.org/");
@@ -37,101 +53,53 @@ public class Testcase06 {
         }
 
         // Step 2: Click on my account link
-        WebElement myAccountLink = driver.findElement(By.linkText("MY ACCOUNT"));
-        myAccountLink.click();
-
-        // Add sleep
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        driver.findElement(myAccountLink).click();
+        Thread.sleep(500);
 
         // Step 3: Login in application using previously created credential
         LoginForm loginForm = new LoginForm(driver);
         loginForm.fillForm("idbbvjciqabfci@gmail.com", "password123");
-
-        // Add sleep
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Thread.sleep(500);
 
         // Click Login
-        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"send2\"]"));
-        loginButton.click();
-
-        // Add sleep
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        driver.findElement(loginBtn).click();
+        Thread.sleep(500);
 
         // Step 4: Click on MY WISHLIST link
-        WebElement wishlistLink = driver
-                .findElement(By.linkText("MY WISHLIST"));
-        wishlistLink.click();
+        driver.findElement(myWishlistLink).click();
+        Thread.sleep(500);
 
-        // Add sleep
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Step 5: In next page, Click ADD TO CART link
-        WebElement addToCartLink = driver
-                .findElement(By.cssSelector("tr > td.wishlist-cell4.customer-wishlist-item-cart > div > button"));
-        addToCartLink.click();
-
-        // Add sleep
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        // Step 5: In next page, Click ADD TO CART button
+        driver.findElement(addToCartBtn).click();
+        Thread.sleep(500);
 
         // Step 6: Enter general shipping country, state/province and zip for the
         // shipping cost estimate
         CartAddForm cartAddForm = new CartAddForm(driver);
         cartAddForm.fillForm("United States", "Alaska", "99501");
-
-        // Add sleep
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Thread.sleep(500);
 
         // Step 7: Click Estimate
-        WebElement estimateBtn = driver.findElement(By.xpath("//*[@id=\"shipping-zip-form\"]/div/button"));
-        estimateBtn.click();
+        driver.findElement(estimateBtn).click();
+        Thread.sleep(500);
 
         // Step 8: Verify Shipping cost generated
-        WebElement shippingCost = driver
-                .findElement(By.xpath("//*[@id=\"co-shipping-method-form\"]/dl/dd/ul/li/label/span"));
-        System.out.println("Shipping cost is:" + shippingCost.getText());
+        System.out.println("Shipping cost is:" + driver.findElement(shippingCost).getText());
+        Thread.sleep(500);
 
         // Step 9: Select Shipping Cost, Update Total
-        WebElement checkboxShippingCost = driver.findElement(By.xpath("//*[@id=\"s_method_flatrate_flatrate\"]"));
-        checkboxShippingCost.click();
-        WebElement updateTotalBtn = driver.findElement(By.xpath("//*[@id=\"co-shipping-method-form\"]/div/button"));
-        updateTotalBtn.click();
+        driver.findElement(shippingCostSelector).click();
+        driver.findElement(shippingCostAddToTotal).click();
+        Thread.sleep(500);
 
         // Step 10: Verify shipping cost is added to total
-        WebElement netCost = driver
-                .findElement(By.xpath("//*[@id=\"shopping-cart-totals-table\"]/tbody/tr[1]/td[2]/span"));
-        System.out.println("Net cost is:" + netCost.getText());
-        WebElement totalCost = driver
-                .findElement(By.xpath("//*[@id=\"shopping-cart-totals-table\"]/tfoot/tr/td[2]/strong/span"));
-        System.out.println("Total cost is:" + totalCost.getText());
+        System.out.println("Net cost is:" + driver.findElement(priceNet).getText());
+        System.out.println("Total cost is:" + driver.findElement(priceTotal).getText());
+        Thread.sleep(500);
 
         // Step 11: Click "Proceed to Checkout"
-        WebElement checkoutBtn = driver
-                .findElement(By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div/div/div[3]/div/ul/li[1]/button"));
-        checkoutBtn.click();
+        driver.findElement(checkoutProcessBtn).click();
+        Thread.sleep(500);
 
         // Step 12a: Enter Billing Information, and click Continue
         // Enter billing information
@@ -142,9 +110,11 @@ public class Testcase06 {
         } catch (Exception e) {
             System.out.println("User already enter billing information");
         }
+        Thread.sleep(500);
+
         // Continue billing
-        WebElement billingContinueBtn = driver.findElement(By.cssSelector("#billing-buttons-container > button"));
-        billingContinueBtn.click();
+        driver.findElement(billingContinueBtn).click();
+        Thread.sleep(500);
 
         // Step 12b: Enter Shipping Information, and click Continue
         // Enter shipping information
@@ -155,46 +125,31 @@ public class Testcase06 {
                     "99501", "0122345678", "111222");
 
             // Continue shipping
-            WebElement billingContinueContinueBtn = driver
-                    .findElement(By.xpath("//*[@id=\"shipping-buttons-container\"]/button"));
-            billingContinueContinueBtn.click();
+            driver.findElement(shippingContinueBtn).click();
         } catch (Exception e) {
             System.out.println("User already enter shipping information!");
         }
+        Thread.sleep(500);
 
         // Step 13: In Shipping Method, Click Continue
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5L));
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#shipping-method-buttons-container button")))
-                .click();
+        wait.until(ExpectedConditions.elementToBeClickable(shippingMethodBtn)).click();
+        Thread.sleep(500);
 
         // Step 14: In Payment Information select 'Check/Money Order' radio button.
         // Click Continue
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#p_method_checkmo")))
-                .click();
-        WebElement checkboxPaymentMethod = driver.findElement(By.cssSelector("#dt_method_checkmo"));
-        // (new Select(checkboxPaymentMethod)).selectByVisibleText("Check / Money
-        // order");
-        checkboxPaymentMethod.click();
-
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#payment-buttons-container > button")))
-                .click();
-        WebElement continueShippingMethodBtn = driver
-                .findElement(By.cssSelector("#payment-buttons-container > button"));
-        continueShippingMethodBtn.click();
+        wait.until(ExpectedConditions.elementToBeClickable(methodCheckoutMoney)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(methodCheckoutMoneyContinue)).click();
+        Thread.sleep(500);
 
         // Step 15: Click 'PLACE ORDER' button
-        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#review-buttons-container > button")))
-                .click();
-        WebElement placeOrderBtn = driver.findElement(By.cssSelector("#review-buttons-container > button"));
-        placeOrderBtn.click();
+        wait.until(ExpectedConditions.elementToBeClickable(placeOrderBtn)).click();
+        Thread.sleep(500);
 
         // Step 16: Verify Oder is generated. Note the order number
-        wait.until(ExpectedConditions.elementSelectionStateToBe(
-                By.cssSelector("body > div > div > div.main-container.col1-layout > div > div > p:nth-child(3)"),
-                false));
-        WebElement orderNumber = driver.findElement(
-                By.cssSelector("body > div > div > div.main-container.col1-layout > div > div > p:nth-child(3)"));
-        System.out.println(orderNumber.getText());
+        wait.until(ExpectedConditions.elementSelectionStateToBe(orderNumber, false));
+        System.out.println(driver.findElement(orderNumber).getText());
+        Thread.sleep(500);
 
         // Capture screenshot at the end
         File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
