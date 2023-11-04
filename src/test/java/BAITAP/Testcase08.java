14cb115ca51b70d2355702f2f8e8f570e3e412ad
vuongbachdoan <vuongbachdoan@gmail.com>
2023-11-04 09:07:56 +0700

    feat: add display old cost

--- src/test/java/BAITAP/Testcase08.java
+++ src/test/java/BAITAP/Testcase08.java
@@ -25,8 +25,6 @@ public class Testcase08 {
         By myOrdersLink = By.linkText("MY ORDERS");
         By reOrderBtn = By
                 .cssSelector("#my-orders-table > tbody > tr.first.odd > td.a-center.view.last > span > a.link-reorder");
-        By printOrdeBtn = By.cssSelector(
-                "body > div > div > div.main-container.col2-left-layout > div > div.col-main > div > div.page-title.title-buttons > a.link-print");
         By estimateBtn = By.cssSelector("#shipping-zip-form > div > button");
         By shippingCost = By.cssSelector("#co-shipping-method-form > dl > dd > ul > li > label > span");
         By shippingCostSelector = By.cssSelector("#co-shipping-method-form > dl > dd > ul > li > label");
@@ -43,6 +41,9 @@ public class Testcase08 {
         By placeOrderBtn = By.cssSelector("#review-buttons-container > button");
         By orderNumber = By
                 .cssSelector("body > div > div > div.main-container.col1-layout > div > div > p:nth-child(3)");
+        By quantifyInput = By.cssSelector("#shopping-cart-table > tbody > tr > td.product-cart-actions > input");
+        By updateQuantityBtn = By.cssSelector("#shopping-cart-table > tbody > tr > td.product-cart-actions > button");
+        By totalCost = By.cssSelector("#shopping-cart-totals-table > tfoot > tr > td:nth-child(2) > strong > span");
 
         WebDriver driver = driverFactory.getChromeDriver();
         // Step 1: Go to http://live.techpanda.org/
@@ -72,46 +73,58 @@ public class Testcase08 {
         driver.findElement(myOrdersLink).click();
         Thread.sleep(500);
 
-        // Step 5: Click on 'ReOrder'
+        //  Click on 'ReOrder'
         driver.findElement(reOrderBtn).click();
         Thread.sleep(500);
+        System.out.println("Old cost is: " + driver.findElement(totalCost).getText());
 
-        // Step 6: Enter general shipping country, state/province and zip for the
+        // Change quantity
+        driver.findElement(quantifyInput).clear();        
+        driver.findElement(quantifyInput).sendKeys("2");
+        driver.findElement(updateQuantityBtn).click();
+
+        // Step 5: Verify Grant total change
+        System.out.println("Grant total cost is: " + driver.findElement(totalCost).getText());
+
+        // Step 5: Enter general shipping country, state/province and zip for the
         // shipping cost estimate
         CartAddForm cartAddForm = new CartAddForm(driver);
         cartAddForm.fillForm("United States", "Alaska", "99501");
         Thread.sleep(500);
 
-        // Step 7: Click Estimate
+        // Step 6: Complete Billing & Shipping Information
         driver.findElement(estimateBtn).click();
         Thread.sleep(500);
 
-        // Step 8: Verify Shipping cost generated
-        System.out.println("Shipping cost is:" + driver.findElement(shippingCost).getText());
+        // Verify Shipping cost generated
+        System.out.println("Shipping cost is:" +
+        driver.findElement(shippingCost).getText());
         Thread.sleep(500);
 
-        // Step 9: Select Shipping Cost, Update Total
+        // Select Shipping Cost, Update Total
         driver.findElement(shippingCostSelector).click();
         driver.findElement(shippingCostAddToTotal).click();
         Thread.sleep(500);
 
-        // Step 10: Verify shipping cost is added to total
+        // Verify shipping cost is added to total
         System.out.println("Net cost is:" + driver.findElement(priceNet).getText());
-        System.out.println("Total cost is:" + driver.findElement(priceTotal).getText());
+        System.out.println("Total cost is:" +
+        driver.findElement(priceTotal).getText());
         Thread.sleep(500);
 
-        // Step 11: Click "Proceed to Checkout"
+        // Click "Proceed to Checkout"
         driver.findElement(checkoutProcessBtn).click();
         Thread.sleep(500);
 
-        // Step 12a: Enter Billing Information, and click Continue
+        // Enter Billing Information, and click Continue
         // Enter billing information
         try {
-            BillingForm billingForm = new BillingForm(driver);
-            billingForm.fillForm("User", "Mid", "Last", "ABC", "HCM", "HN", "HCM", "United States", "Alaska", "99501",
-                    "0122345678", "111222");
+        BillingForm billingForm = new BillingForm(driver);
+        billingForm.fillForm("User", "Mid", "Last", "ABC", "HCM", "HN", "HCM",
+        "United States", "Alaska", "99501",
+        "0122345678", "111222");
         } catch (Exception e) {
-            System.out.println("User already enter billing information");
+        System.out.println("User already enter billing information");
         }
         Thread.sleep(500);
 
@@ -119,37 +132,37 @@ public class Testcase08 {
         driver.findElement(billingContinueBtn).click();
         Thread.sleep(500);
 
-        // Step 12b: Enter Shipping Information, and click Continue
+        // Enter Shipping Information, and click Continue
         // Enter shipping information
         try {
-            ShippingForm shippingInformation = new ShippingForm(driver);
-            shippingInformation.fillForm("User", "Mid", "Last", "ABC", "HCM", "HN",
-                    "HCM", "United States", "Alaska",
-                    "99501", "0122345678", "111222");
+        ShippingForm shippingInformation = new ShippingForm(driver);
+        shippingInformation.fillForm("User", "Mid", "Last", "ABC", "HCM", "HN",
+        "HCM", "United States", "Alaska",
+        "99501", "0122345678", "111222");
 
-            // Continue shipping
-            driver.findElement(shippingContinueBtn).click();
+        // Continue shipping
+        driver.findElement(shippingContinueBtn).click();
         } catch (Exception e) {
-            System.out.println("User already enter shipping information!");
+        System.out.println("User already enter shipping information!");
         }
         Thread.sleep(500);
 
-        // Step 13: In Shipping Method, Click Continue
+        // In Shipping Method, Click Continue
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5L));
         wait.until(ExpectedConditions.elementToBeClickable(shippingMethodBtn)).click();
         Thread.sleep(500);
 
-        // Step 14: In Payment Information select 'Check/Money Order' radio button.
+        // In Payment Information select 'Check/Money Order' radio button.
         // Click Continue
         wait.until(ExpectedConditions.elementToBeClickable(methodCheckoutMoney)).click();
         wait.until(ExpectedConditions.elementToBeClickable(methodCheckoutMoneyContinue)).click();
         Thread.sleep(500);
 
-        // Step 15: Click 'PLACE ORDER' button
+        // Click 'PLACE ORDER' button
         wait.until(ExpectedConditions.elementToBeClickable(placeOrderBtn)).click();
         Thread.sleep(500);
 
-        // Step 16: Verify Oder is generated. Note the order number
+        // Step 7: Verify Oder is generated. Note the order number
         wait.until(ExpectedConditions.elementSelectionStateToBe(orderNumber, false));
         System.out.println(driver.findElement(orderNumber).getText());
         Thread.sleep(500);
@@ -158,11 +171,12 @@ public class Testcase08 {
         File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 
         try {
-            FileUtils.copyFile(scrFile, new File("src/main/resources/screenshots/screenshot_tc08.png"));
-            System.out.println("Screenshot captured!");
+        FileUtils.copyFile(scrFile, new
+        File("src/main/resources/screenshots/screenshot_tc08.png"));
+        System.out.println("Screenshot captured!");
         } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Failed to capture screenshot!");
+        e.printStackTrace();
+        System.out.println("Failed to capture screenshot!");
         }
 
         // Close the browser
